6df30eff9e171cadd95f8a131ef625be5722f570
Manish Shrestha <9460504+shrestaz@users.noreply.github.com>
2022-07-17 01:21:13 +0200

    Reproduction of Link issue

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+
+from services import create_link_token, retrieve_account_token
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+    "http://localhost:63342"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/create-link-token")
+def get_link_token():
+    token = create_link_token()
+    return token
+
+
+@app.get("/retrieve_account_token/{public_token}")
+def get_account_token(public_token: str):
+    print(public_token)
+    account_token = retrieve_account_token(public_token)
+    return account_token
---
+++ services.py
@@ -0,0 +1,38 @@
+import requests
+
+api_key = "YOUR_API_KEY"
+
+# Replace api_key with your Merge production API Key
+def create_link_token():
+    body = {
+        "end_user_origin_id": '',  # unique entity ID
+        "end_user_organization_name": '',  # your user's organization name
+        "end_user_email_address": '',  # your user's email address
+        "categories": ["hris"],  # choose your category
+    }
+
+    headers = {"Authorization": f"Bearer {api_key}"}
+
+    link_token_url = "https://api.merge.dev/api/integrations/create-link-token"
+    link_token_result = requests.post(link_token_url, data=body, headers=headers)
+    link_token = link_token_result.json().get("link_token")
+
+    return link_token
+
+
+def retrieve_account_token(public_token):
+    headers = {"Authorization": f"Bearer {api_key}"}
+
+    account_token_url = "https://api.merge.dev/api/integrations/account-token/{}".format(public_token)
+    account_token_result = requests.get(account_token_url, headers=headers)
+
+    account_token = account_token_result.json().get("account_token")
+    return account_token  # Save this in your database
+
+
+"""
+Notes:
+    uoi: f208e489-fafe-4de0-a231-b722042ce4ff
+    uon: Dataminr Test User GZCLQS61
+    uae: pgalfidmcom
+    """
